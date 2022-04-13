6e6b657e562d0a000aa820e967e6548890488d32
LichVayne <sandro.mekoshkishvili.1@Iliauni.edu.ge>
2022-04-13 17:34:33 +0400

    Update

---
+++ src/ArgumentParser.java
@@ -0,0 +1,9 @@
+public interface ArgumentParser {
+    public static final String CREATE_STUDENT = "createStudent";
+    public static final String CREATE_COURSE = "createCourse";
+    public static final String CREATE_ASSISTANCE = "createAssistance";
+    public static final String CREATE_PROFESSOR = "createProfessor";
+    public static final String ASIGHSTUDENTTOCOURSE = "asighStudentToCourse";
+    public static final String ASIGHASSISTANCETOCOURSE = "asighAssistanceToCourse";
+    public static final String ASIGHPROFESSORTOCOURSE = "asighProfessorToCourse";
+}
